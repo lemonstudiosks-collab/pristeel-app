3ee12bff46273b651f2dad8e5c6320f08d35d7ff
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-01 07:51:30 +0200

    Load Gmail multi-project linking UI

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -16,6 +16,7 @@ var files=[
   'pristeel-gmail-open-project.js?v=20260801-1',
   'pristeel-email-relations.js?v=20260801-1',
   'pristeel-project-contacts.js?v=20260801-1',
+  'pristeel-email-multi-link-ui.js?v=20260801-1',
   'pristeel-ui-v2.js?v=20260801-1',
   'pristeel-ui-session.js?v=20260801-1',
   'pristeel-ui-v2-polish.js?v=20260801-1',
