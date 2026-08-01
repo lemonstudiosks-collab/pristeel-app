6d50d44cd388a07bab747cb092f5a99ccba3c3db
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-01 19:32:51 +0200

    Refresh Gmail intake single-instance bypass

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -2,7 +2,7 @@
 (function(){
 'use strict';
 var files=[
-  'pristeel-single-instance.js?v=20260731-1',
+  'pristeel-single-instance.js?v=20260801-2',
   'pristeel-email-core.js?v=20260801-2',
   'pristeel-gmail-auth-gate.js?v=20260801-1',
   'pristeel-email-outreach.js?v=20260801-1',
