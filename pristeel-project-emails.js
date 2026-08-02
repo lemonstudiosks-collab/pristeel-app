4162607c8ecc95b7f7f9deb7dc48f3e88c18cf48
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-02 08:32:12 +0200

    Load project Gmail collector

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -21,6 +21,7 @@ var files=[
   'pristeel-email-relations.js?v=20260801-1',
   'pristeel-project-contacts.js?v=20260801-1',
   'pristeel-email-multi-link-ui.js?v=20260801-1',
+  'pristeel-project-gmail-collector.js?v=20260802-1',
   'pristeel-gmail-audit.js?v=20260801-1',
   'pristeel-project-discovery.js?v=20260801-1',
   'pristeel-project-discovery-create-fix.js?v=20260801-1',
