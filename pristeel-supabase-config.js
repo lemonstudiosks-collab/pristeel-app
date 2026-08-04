76f999323086d19d22c3c733d8ab9c8b2c93e793
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-04 07:58:24 +0200

    Add canonical Supabase public configuration

---
+++ pristeel-supabase-config.js
@@ -0,0 +1,6 @@
+/* PRISTEEL canonical Supabase public configuration */
+(function(){
+'use strict';
+window._SB_URL='https://isymxqfqzkchbsrbhucf.supabase.co';
+window._SB_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeW14cWZxemtjaGJzcmJodWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NDU1NzYsImV4cCI6MjA5ODIyMTU3Nn0.H25Z7TSVv0OD0X1QPqlowAr0uLSo88_Bu7R_cW6KAIM';
+})();
