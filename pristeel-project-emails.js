9408aaf54abcdbc2a51f63be39643abcf27be88e
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-05 19:38:09 +0200

    Refresh direct project opener with Gmail action

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -71,7 +71,7 @@ var files=[
   'pristeel-project-bulk-gmail-recovery-v1.js?v=20260805-1',
   'pristeel-project-closure-direct-v1.js?v=20260805-1',
   'pristeel-project-flow-actions-v1.js?v=20260805-2',
-  'pristeel-project-open-direct-v1.js?v=20260805-1'
+  'pristeel-project-open-direct-v1.js?v=20260805-2'
 ];
 var completed=false;
 function ready(){
