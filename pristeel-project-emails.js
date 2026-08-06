0fca71fea0890024152e5d7e64812684bc180d02
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-06 11:33:46 +0200

    Refresh homepage project card cache version

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -69,7 +69,7 @@ var files=[
   'pristeel-project-flow-actions-v1.js?v=20260805-2',
   'pristeel-project-open-direct-v1.js?v=20260805-2',
   'pristeel-project-contacts-full-v1.js?v=20260805-1',
-  'pristeel-dashboard-task-cards-v1.js?v=20260806-2'
+  'pristeel-dashboard-task-cards-v1.js?v=20260806-3'
 ];
 var completed=false;
 function ready(){
