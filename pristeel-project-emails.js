0ba6c9f4a12fa4e0c188a55f31f0bff062f81eaf
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-06 10:04:28 +0200

    Load isolated dashboard task cards last

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -68,7 +68,8 @@ var files=[
   'pristeel-project-closure-direct-v1.js?v=20260805-1',
   'pristeel-project-flow-actions-v1.js?v=20260805-2',
   'pristeel-project-open-direct-v1.js?v=20260805-2',
-  'pristeel-project-contacts-full-v1.js?v=20260805-1'
+  'pristeel-project-contacts-full-v1.js?v=20260805-1',
+  'pristeel-dashboard-task-cards-v1.js?v=20260806-1'
 ];
 var completed=false;
 function ready(){
