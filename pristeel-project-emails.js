15201eed344002ed5b57185b255b6309c5c27bf7
lemonstudiosks-collab <lemonstudios.ks@gmail.com>
2026-08-06 07:44:34 +0200

    Load direct Gmail launch without tab closing

--- pristeel-project-emails.js
+++ pristeel-project-emails.js
@@ -2,7 +2,7 @@
 (function(){
 'use strict';
 var files=[
-  'pristeel-gmail-tab-handoff.js?v=20260806-1',
+  'pristeel-gmail-tab-handoff.js?v=20260806-2',
   'pristeel-email-core.js?v=20260801-2',
   'pristeel-google-workspace-auth.js?v=20260802-2',
   'pristeel-project-routing-rules.js?v=20260802-2',
